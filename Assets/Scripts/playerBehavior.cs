d8895ad15bc7a49cfb15001d3006ffc044d01718
course-10208-Ex-001 <orian43@gmail.com>
2026-09-11 14:01:44 +0300

    Moving the player with the mouse

--- Assets/Scripts/playerBehavior.cs
+++ Assets/Scripts/playerBehavior.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    private float speed = 3;
+    public GameObject PlayerCamera; // public means that it must be connected in Unity
+    float speed = 3;
+    float angular_speed = 100;
     CharacterController controller;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         controller = GetComponent<CharacterController>(); //initialization
-
-
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+        Debug.Log("GPU: " + SystemInfo.graphicsDeviceName);
     }
 
     // Update is called once per frame
@@ -17,7 +20,11 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         // נרמול המהירות, שהיא תיהיה אחידה
         float dx=0, dz=0;
+        float RotationAboutY = Input.GetAxis("Mouse X")* angular_speed*Time.deltaTime;
+        float RotationAboutX = -Input.GetAxis("Mouse Y") * 0.2f*angular_speed * Time.deltaTime;
 
+        PlayerCamera.transform.Rotate(RotationAboutX, 0, 0);
+        transform.Rotate(new Vector3(0, RotationAboutY,0));
         dz = Input.GetAxis("Vertical")*speed*Time.deltaTime;
         dx = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 
@@ -28,8 +35,9 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
         // הזזה בתחשבות במכשולים
 
-        Vector3 motion = new Vector3(dx, 0, dz);
-        controller.Move(motion);
+        Vector3 motion = new Vector3(dx, -1, dz);
+        motion = transform.TransformDirection(motion); // transforms mation to global coordinates
+        controller.Move(motion); //global coordinates
 
     }
 }
